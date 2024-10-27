8e2567d9635dd9e7b49e15e52f20a0dc33d32ae4
Levan Ilashvili <levan@ilashvili.me>
2024-10-27 09:29:31 +0400

    define state - initial version

--- src/state.rs
+++ src/state.rs
@@ -1,1 +1,15 @@
+use cosmwasm_std::Addr;
+use cw_storage_plus::Map;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct FractionalNFT {
+    pub owner: Addr,            // originl owner
+    pub nft_contract: Addr,     // nft contract addr
+    pub token_id: String,       // nft id    
+    pub total_shares: Uint128,  // fraction counter
+    pub cw20_contract: Addr,    // fraction contract
+}
+
+pub const FRACTIONAL_NFTS: Map<(&Addr, &str), FractionalNFT> = Map::new("fractional_nfts");
